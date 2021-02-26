367396099f628943fd2e3f8fb81cfcf8b3be045b
Serg <romakinsv@gmail.com>
2021-02-27 01:12:20 +0300

    Add security annotations

--- src/main/java/com/example/demo/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/demo/configuration/WebSecurityConfig.java
@@ -2,12 +2,14 @@ package com.example.demo.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -17,7 +19,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("user").password(enc.encode("pass")).roles("READ")
                 .and()
-                .withUser("manager").password(enc.encode("passw0rd")).roles("READ", "WRITE");
+                .withUser("manager").password(enc.encode("passw0rd")).roles("WRITE")
+                .and()
+                .withUser("admin").password(enc.encode("passw0rd1")).roles("WRITE", "DELETE");
     }
 
     @Override
@@ -27,8 +31,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests().antMatchers("/persons/hello").permitAll()
                 .and()
                 .authorizeRequests()
-                    .antMatchers("/persons/get-**").hasRole("READ")
-                    .antMatchers("/persons/set", "/persons/delete").hasRole("WRITE")
+//                    .antMatchers("/persons/get-**").hasRole("READ")
+//                    .antMatchers("/persons/set", "/persons/delete").hasRole("WRITE")
                     .anyRequest().authenticated();
     }
 }
--- src/main/java/com/example/demo/controller/DataController.java
+++ src/main/java/com/example/demo/controller/DataController.java
@@ -2,8 +2,14 @@ package com.example.demo.controller;
 
 import com.example.demo.dao.Person;
 import com.example.demo.service.PersonsService;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,7 +29,14 @@ public class DataController {
         return "Hi, guest!";
     }
 
+    @GetMapping("/userDetails")
+    @PostAuthorize("returnObject.username == 'user' or hasRole('DELETE')")
+    public User userDetails() {
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
     @GetMapping("/set")
+    @Secured("WRITE")
     public boolean createOrUpdatePerson(
             @RequestParam("name") String name,
             @RequestParam("surname") String surname,
@@ -38,6 +51,7 @@ public class DataController {
     }
 
     @GetMapping("/delete")
+    @Secured("DELETE")
     public boolean deletePerson(
             @RequestParam("name") String name,
             @RequestParam("surname") String surname,
@@ -47,17 +61,19 @@ public class DataController {
     }
 
     @GetMapping("/get-by-city")
+    @Secured("READ")
     public List<Person> getPersonsByCity(@RequestParam("city") String city) {
         return personsService.getPersonsByCity(city);
     }
 
     @GetMapping("/get-age-more-than")
-    @ResponseBody
+    @PreAuthorize("hasRole('READ') or hasRole('WRITE')")
     public List<Person> getPersonsAgeMoreThan(@RequestParam("age") int age) {
         return personsService.getPersonsAgeMoreThan(age);
     }
 
     @GetMapping("/get-by-name-and-surname")
+    @RolesAllowed({"READ", "WRITE", "DELETE"})
     public Optional<Person> getPersonsByNameAndSurname(@RequestParam("name") String name, @RequestParam("surname") String surname) {
         return personsService.getPersonsByNameAndSurname(name, surname);
     }
